4baa41a9c15f9b20d62e777e1c7ac1addffd0528
陈凯 <chen526110979@qq.com>
2017-03-22 18:16:24 +0800

    添加了GPUImage静态库(真机和模拟器)

--- GPUImageUsage/Controller/ViewController.m
+++ GPUImageUsage/Controller/ViewController.m
@@ -8,10 +8,12 @@
 
 #import "ViewController.h"
 #import "CardLayOut.h"
+#import "GPUImage.h"
 
 @interface ViewController () <UICollectionViewDataSource>
 
 @property (strong, nonatomic) UICollectionView *collectionView;
+@property (strong, nonatomic) UIImageView *imageView;
 
 @end
 
@@ -20,6 +22,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
+    self.imageView.image = [UIImage imageNamed:@"testbg.png"];
+    [self.view addSubview:self.imageView];
+    
+    /*
     CardLayOut *layout = [CardLayOut new];
     
     self.collectionView = [[UICollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
@@ -27,12 +34,28 @@
     [self.view addSubview:self.collectionView];
     
     [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"CellID"];
+     */
 }
 
 - (void)viewDidLayoutSubviews {
     [super viewDidLayoutSubviews];
     self.collectionView.frame = self.view.bounds;
     
+    self.imageView.frame = self.view.bounds;
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"点击了");
+    GPUImageSketchFilter *filter = [[GPUImageSketchFilter alloc] init];
+    [filter forceProcessingAtSize:self.imageView.bounds.size];
+    [filter useNextFrameForImageCapture];
+    
+    GPUImagePicture *picture = [[GPUImagePicture alloc] initWithImage:self.imageView.image];
+    [picture addTarget:filter];
+    [picture processImage];
+    
+    UIImage *newImage = [filter imageFromCurrentFramebuffer];
+    self.imageView.image = newImage;
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
